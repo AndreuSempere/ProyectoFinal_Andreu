5052e1a9ecf43031f2c09e4a019b991a112360fc
Andreu <andreusm1707@gmail.com>
2025-01-30 15:24:06 +0100

    Mejoras sobre Huella Digital

--- flutter_bank_app/lib/Presentation/Widgets/Auth/Login/sign_in_form.dart
+++ flutter_bank_app/lib/Presentation/Widgets/Auth/Login/sign_in_form.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bank_app/Presentation/Blocs/auth/login_bloc.dart';
 import 'package:flutter_bank_app/Presentation/Blocs/auth/login_event.dart';
-import 'package:flutter_bank_app/Presentation/Blocs/auth/login_state.dart';
 import 'package:flutter_bank_app/Presentation/Blocs/biometric/biometric_auth_bloc.dart';
 import 'package:flutter_bank_app/Presentation/Blocs/biometric/biometric_auth_event.dart';
 import 'package:flutter_bank_app/Presentation/Blocs/biometric/biometric_auth_state.dart';
@@ -51,94 +50,99 @@ class _SignInFormState extends State<SignInForm> {
 
   @override
   Widget build(BuildContext context) {
-    return BlocListener<LoginBloc, LoginState>(
-      listener: (context, state) {
-        if (state.message != null) {
-          _showSnackbar(
-            context,
-            state.message ??
-                AppLocalizations.of(context)!.errorRegistrarUsuario,
-          );
-        }
-        if (state.email != null) {
-          ScaffoldMessenger.of(context).showSnackBar(
-            const SnackBar(content: Text('Login successful!')),
-          );
-          context.go('/home');
-        }
-      },
-      child: Form(
-        key: _formKey,
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              _buildLabel(context, AppLocalizations.of(context)!.email),
-              _buildTextField(
-                controller: _emailController,
-                icon: "assets/icons/email.svg",
-                validator: (value) => value!.isEmpty
-                    ? AppLocalizations.of(context)!.elNombreEsObligatorio
-                    : null,
-              ),
-              const SizedBox(height: 12),
-              _buildLabel(context, AppLocalizations.of(context)!.password),
-              _buildTextField(
-                controller: _passwordController,
-                icon: "assets/icons/password.svg",
-                obscureText: true,
-                validator: (value) => value!.isEmpty
-                    ? AppLocalizations.of(context)!.elNombreEsObligatorio
-                    : null,
-              ),
-              const SizedBox(height: 18),
-              ElevatedButton(
-                onPressed: () => signIn(context),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color(0xFFF77D8E),
-                  minimumSize: const Size(double.infinity, 45),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                ),
-                child: Text(
-                  AppLocalizations.of(context)!.textbuttonlogin,
-                  style: const TextStyle(color: Colors.white, fontSize: 16),
-                ),
-              ),
-              const SizedBox(height: 12),
-              BlocBuilder<BiometricAuthBloc, BiometricAuthState>(
-                builder: (context, state) {
-                  if (state is BiometricAuthInitial) {
-                    context
-                        .read<BiometricAuthBloc>()
-                        .add(LoadBiometricStatus());
-                    return const SizedBox.shrink();
-                  }
+    context.read<LoginBloc>().stream.listen((state) {
+      if (state.email != null) {
+        context.go('/home');
+      } else if (state.message != null) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+              content: Text(state.message ??
+                  AppLocalizations.of(context)!.errorRegistrarUsuario)),
+        );
+      }
+    });
 
-                  if (state is BiometricStatusLoaded && state.isEnabled) {
-                    return ElevatedButton.icon(
-                      onPressed: () => signInWithBiometrics(context),
-                      icon: const Icon(Icons.fingerprint),
-                      label: Text(
-                        AppLocalizations.of(context)!.textbuttonloginBiometrico,
-                        style: const TextStyle(color: Colors.white),
-                      ),
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color(0xFFF77D8E),
-                        minimumSize: const Size(double.infinity, 45),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(15),
-                        ),
-                      ),
-                    );
-                  }
-
+    context.read<BiometricAuthBloc>().stream.listen((state) {
+      if (state is BiometricLoginSuccess) {
+        context.read<LoginBloc>().add(
+              LoginButtonPressed(
+                email: state.email,
+                password: state.password,
+              ),
+            );
+      } else if (state is BiometricAuthError) {
+        _showSnackbar(context, state.message);
+      }
+    });
+    return Form(
+      key: _formKey,
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _buildLabel(context, AppLocalizations.of(context)!.email),
+            _buildTextField(
+              controller: _emailController,
+              icon: "assets/icons/email.svg",
+              validator: (value) => value!.isEmpty
+                  ? AppLocalizations.of(context)!.elNombreEsObligatorio
+                  : null,
+            ),
+            const SizedBox(height: 12),
+            _buildLabel(context, AppLocalizations.of(context)!.password),
+            _buildTextField(
+              controller: _passwordController,
+              icon: "assets/icons/password.svg",
+              obscureText: true,
+              validator: (value) => value!.isEmpty
+                  ? AppLocalizations.of(context)!.elNombreEsObligatorio
+                  : null,
+            ),
+            const SizedBox(height: 18),
+            ElevatedButton(
+              onPressed: () => signIn(context),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color(0xFFF77D8E),
+                minimumSize: const Size(double.infinity, 45),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+              ),
+              child: Text(
+                AppLocalizations.of(context)!.textbuttonlogin,
+                style: const TextStyle(color: Colors.white, fontSize: 16),
+              ),
+            ),
+            const SizedBox(height: 12),
+            BlocBuilder<BiometricAuthBloc, BiometricAuthState>(
+              builder: (context, state) {
+                if (state is BiometricAuthInitial) {
+                  context.read<BiometricAuthBloc>().add(LoadBiometricStatus());
                   return const SizedBox.shrink();
-                },
-              ),
-            ],
-          ),
+                }
+
+                if (state is BiometricStatusLoaded && state.isEnabled) {
+                  return ElevatedButton.icon(
+                    onPressed: () => signInWithBiometrics(context),
+                    icon: const Icon(Icons.fingerprint),
+                    label: Text(
+                      AppLocalizations.of(context)!.textbuttonloginBiometrico,
+                      style: const TextStyle(color: Colors.white),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFFF77D8E),
+                      minimumSize: const Size(double.infinity, 45),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                    ),
+                  );
+                }
+
+                return const SizedBox.shrink();
+              },
+            ),
+          ],
         ),
       ),
     );
